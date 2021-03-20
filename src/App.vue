f456f0918858627ea09022f5dc59aad35c0516ab
Leonardo <leolama18@gmail.com>
2021-03-19 22:56:55 -0400

    holis

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      dense
+      color="deep-purple accent-4"
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>MalditoEsteban</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-main>
+      <contenedor-coso/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import ContenedorCoso from './components/ContenedorCoso';
+
+export default {
+  name: 'App',
+  components: {
+    ContenedorCoso,
+  },
+};
+</script>
---
+++ src/components/ContenedorCoso.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/img/Bluetooth.png')"
+          class="my-3"
+          contain
+          height="9rem"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Hola... pelawebo...
+        </h1>
+      </v-col>
+      <v-col cols="12">
+        <v-dialog
+          v-model="dialog"
+          persistent
+          max-width="290"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              v-bind="attrs"
+              v-on="on"
+            >
+              Tocame
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title class="headline">
+              Esteban es puto?
+            </v-card-title>
+            <v-card-text>Si lo es</v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="green darken-1"
+                text
+                @click="dialog = false"
+              >
+                Cerrar
+              </v-btn>
+              <v-btn
+                color="green darken-1"
+                text
+                @click="dialog=false,loading=true"
+              >
+                Aceptar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+      <v-col cols="12">
+        <v-progress-linear
+          indeterminate
+          color="cyan"
+          v-show="(this.loading)"
+        ></v-progress-linear>
+      </v-col>
+    </v-row>
+    
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        dialog: false,
+        loading: false
+      }
+    },
+    methods: {
+    }
+  }
+</script>
